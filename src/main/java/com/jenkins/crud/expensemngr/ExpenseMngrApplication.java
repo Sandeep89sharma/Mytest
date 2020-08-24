0b4fd3d22fac522961f13f4a28103affc52b480d
Sandeep_Sharma32 <Sandeep.sharma@team.telstra.com>
2020-08-24 14:43:42 +0530

    first commit

---
+++ src/main/java/com/jenkins/crud/expensemngr/ExpenseMngrApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.crud.expensemngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMngrApplication {
+
+    public static void main(String args[])
+    {
+        SpringApplication.run(ExpenseMngrApplication.class,args);
+    }
+}
